85078f6619d736ecb347c3b3b4c5520194f62565
Dan <dan93c@yahoo.com>
2014-10-19 19:58:15 +0300

    First commit. Immortals

---
+++ src/main/java/ro/immortals/controller/MainController.java
@@ -0,0 +1,19 @@
+package ro.immortals.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView("login");
+		modelAndView.addObject("message","You're the next one! Ha ha ha");
+		return modelAndView;
+	}
+}
